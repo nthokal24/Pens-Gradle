5a165c38e8e97422f2f217133f770cbeec721395
nthokal24 <nthokal3@gmail.com>
2024-12-23 19:05:46 +0530

    test

---
+++ src/main/java/com/pen/PensGradleApplication.java
@@ -0,0 +1,13 @@
+package com.pen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensGradleApplication.class, args);
+	}
+
+}
